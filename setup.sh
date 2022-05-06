7e6a549b711043cd72be4b75352165c21dfbadec
Wang CHEN <solid.chen9@gmail.com>
2022-05-06 15:16:52 +0200

    add color theme in setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,9 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]\n\
+primaryColor = \"#fa0057\"\n\
+backgroundColor = \"#FFFFFF\"\n\
+secondaryBackgroundColor = \"#fofofo\"\n\
+textColor = \"#003684\"\n\
 " > ~/.streamlit/config.toml
