55be141bfb05b6805116ad44a78970d64732e791
WangCHEN9 <solid.chen9@gmail.com>
2022-05-19 10:22:27 +0200

    remove datainfo cache

--- pages/data_info.py
+++ pages/data_info.py
@@ -2,7 +2,6 @@ import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-@st.cache(suppress_st_warning=True)
 def write(state_df):
     st.header("Data Explotary Analysis")
     
